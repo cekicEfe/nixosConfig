28144da4d88ac966a843598292643fb1abdd1b3e
cekicEfe <efecekic0@gmail.com>
2025-10-19 17:32:07 +0300

    Newer version

---
+++ NixShells/racketTemplate/flake.nix
@@ -0,0 +1,29 @@
+{
+
+  description = "Racket development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = pkgs.mkShell {
+
+          # Dependencies for project
+          buildInputs = [ pkgs.racket pkgs.raco ];
+
+          #links libraries to shell
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+            #
+          ];
+
+          shellHook = ''
+            PS1="[\\u@\\h && RACKET-DEV-ENV:\\w]\$ "
+          '';
+        };
+      });
+}
