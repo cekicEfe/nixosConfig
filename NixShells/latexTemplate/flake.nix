2244c7bc4a81ebb3fbe5172a84acb2139700e65c
cekicEfe <efecekic0@gmail.com>
2025-11-10 00:28:26 +0300

    Updated LaTeX

--- NixShells/latexTemplate/flake.nix
+++ NixShells/latexTemplate/flake.nix
@@ -8,25 +8,23 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
+      let
+        pkgs = import nixpkgs { inherit system; };
+        tex = (pkgs.texlive.combine {
+          inherit (pkgs.texlive)
+            scheme-full dvisvgm dvipng # for preview and export as html
+            wrapfig amsmath ulem hyperref capt-of;
+          #(setq org-latex-compiler "lualatex")
+          #(setq org-preview-latex-default-process 'dvisvgm)
+        });
       in {
         devShell = pkgs.mkShell {
 
           # Dependencies for project
-          buildInputs = [
-            pkgs.texliveTeTeX
-            pkgs.texlab
-            #pkgs.texstudio
-            #pkgs.texmaker
-          ];
+          buildInputs = [ tex pkgs.texlab pkgs.texstudio pkgs.texmaker ];
 
           #links libraries to shell
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-            pkgs.texliveTeTeX
-            pkgs.texlab
-            #pkgs.texstudio
-            #pkgs.texmaker
-          ];
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ ];
 
           shellHook = ''
             PS1="[\\u@\\h && LATEX-DEV-ENV:\\w]\$ "
--- desktop1/default.nix
+++ desktop1/default.nix
@@ -1,15 +1,15 @@
 {
   imports = [
-    ../common/configuration.nix
+    ../common/nixSettings.nix
+    ../common/localTr.nix
 
     ../common/packages.nix
     ../common/modules
 
     ../common/users/users.nix
 
-    ../common/display-protocol/window-managers/i3/normal/i3.nix
+    ../common/display-protocol/window-managers/i3/fancy/i3.nix
     ../common/display-protocol/desktop-managers/xfce4/xfce4.nix
-    #../common/display-protocol/premade/premadeKde.nix
 
     ./extraConfig
 
