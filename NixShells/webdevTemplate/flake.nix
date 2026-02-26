f9951d4fe6b6af4ede18f6381cfd8dac1bbf47ac
cekicEfe <efecekic0@gmail.com>
2026-02-27 00:49:47 +0300

    Added webdev template

---
+++ NixShells/webdevTemplate/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Web development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = pkgs.mkShell {
+
+          # Dependencies for project
+          buildInputs = [
+            #
+            pkgs.nodejs
+            pkgs.dotnet-aspnetcore
+            pkgs.dotnet-sdk
+            pkgs.dotnetCorePackages.dotnet_10
+          ];
+
+          #links libraries to shell
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+            #
+          ];
+
+          shellHook = ''
+            PS1="[\\u@\\h && WEB-DEV-ENV:\\w]\$ "
+          '';
+        };
+      });
+}
