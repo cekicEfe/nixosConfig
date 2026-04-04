c29eedffbad2c0fab9608b20d17a18992c9826d1
cekicEfe <efecekic0@gmail.com>
2026-04-04 11:48:31 +0300

    Made desktop1 config prettier

--- desktop1/default.nix
+++ desktop1/default.nix
@@ -1,21 +1,21 @@
 {
   imports = [
-    ../common/nixSettings.nix
-    ../common/localTr.nix
-
     ../common/packages.nix
     ../common/modules
 
-    ../common/users/users.nix
+    ../common/localTr.nix    
+    ../common/nixSettings.nix
 
     ../common/display-protocol/window-managers/i3/fancy/i3.nix
-    ../common/display-protocol/display-managers/sddm/sddm.nix
-    ../common/display-protocol/desktop-managers/plasma6/plasma6.nix
-
+    #../common/display-protocol/display-managers/sddm/sddm.nix
+    #../common/display-protocol/desktop-managers/plasma6/plasma6.nix
+    ../common/display-protocol/desktop-managers/xfce4/xfce4.nix
+    
     ../common/msp430-udev.nix
-    ./extraConfig
-
     ../common/kernelOptions
+    ../common/users/users.nix
+
+    ./extraConfig
     ./bootloader/bootloader.nix
     ./hardware-configuration.nix
   ];
